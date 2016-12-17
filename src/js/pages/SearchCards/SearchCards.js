89bc775a452924113551846f26d96bcc932374ba
Eduardo Martinez Ruiz <dwardmr@gmail.com>
2016-12-17 00:24:57 -0600

    Added card quality checkboxes and inlineStyles for SearchCards page

--- src/js/pages/SearchCards/SearchCards.js
+++ src/js/pages/SearchCards/SearchCards.js
@@ -3,6 +3,7 @@ import Divider from 'material-ui/Divider';
 import RaisedButton from 'material-ui/RaisedButton';
 import Search from 'material-ui/svg-icons/action/Search';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import Checkbox from 'material-ui/Checkbox';
 
 import SearchBar from '../../components/SearchBar/SearchBar';
 import SelectItems from '../../components/SelectItems/SelectItems';
@@ -12,65 +13,95 @@ import styles from './style.css'
 export default class SearchCards extends React.Component{
 	render(){
 		const localeArray= [{"labelText": "enUS - American English "}, {"labelText": "enGB - European English"}, {"labelText": "deDE - German"}, {"labelText": "esES - European Spanish"}, {"labelText": "esMX - Latin American Spanish"}, {"labelText": "frFR - French"}, {"labelText": "itIT - Italian"}, {"labelText": "koKR - Korean"}, {"labelText": "plPL - Polish"}, {"labelText": "ptBR - Brazilian Portuguese"}, {"labelText": "ruRU - Russian"}, {"labelText": "zhCN - Simplified Chinese"}, {"labelText": "zhTW - Traditional Chinese"}, {"labelText": "jaJP - Japanese"}, {"labelText": "thTH - Thai"}];
-		const cardSets= [{"labelText": "Basic"}, {"labelText": "Classic"}, {"labelText": "Promo"},  {"labelText": "Reward"}, {"labelText": "Naxxramas"}, {"labelText": "Goblins vs Gnomes"}, {"labelText": "Blackrock Mountain"}, {"labelText": "The Grand Tournament"}, {"labelText": "The League of Explorers"}, {"labelText": "Whispers of the Old Gods"}, {"labelText": "Karazhan"}, {"labelText": "Mean Streets of Gadgetzan"}];
+		const cardSets= [{"labelText": "Any"}, {"labelText": "Basic"}, {"labelText": "Classic"}, {"labelText": "Promo"},  {"labelText": "Reward"}, {"labelText": "Naxxramas"}, {"labelText": "Goblins vs Gnomes"}, {"labelText": "Blackrock Mountain"}, {"labelText": "The Grand Tournament"}, {"labelText": "The League of Explorers"}, {"labelText": "Whispers of the Old Gods"}, {"labelText": "Karazhan"}, {"labelText": "Mean Streets of Gadgetzan"}];
+		const cardQuality= [{"quality": "Free"}, {"quality": "Common"}, {"quality": "Rare"}, {"quality": "Epic"}, {"quality": "Legandary"}];
+		const inlineStyles = {
+			checkboxIcon: {
+				marginRight: "5px",
+			}
+		};
+		const qualityCheckboxes = cardQuality.map((qItem, i) =>
+			<Checkbox
+				key={i}
+				label={qItem.quality}	
+				iconStyle={inlineStyles.checkboxIcon}
+			/>
+		);
 		return(
 			<div>
 				<h2>Search Cards!</h2>
-				<Divider />
 				<p>Go through the Innkeeper's finest commons.</p>
-				<div class={styles.dataContainer}>
-					<div class={styles.inputContainerLarge}>
-						<SearchBar
-							placeholderText="Seach a Card by Name"
-							borderColor="#FF5722"
-						/>
-					</div>
-				</div>
-				<div class={styles.dataContainer}>
-					<div class={styles.inputContainerNumber}>
-						<SearchBar
-							placeholderText="Mana Cost"
-							borderColor="#FF5722"
-						/>
-					</div>
-					<div class={styles.spaceDiv} />
-					<div class={styles.inputContainerNumber}>
-						<SearchBar
-							placeholderText="Attack"
-							borderColor="#FF5722"
-						/>
-					</div>
-					<div class={styles.spaceDiv} />
-					<div class={styles.inputContainerNumber}>
-						<SearchBar
-							placeholderText="Health"
-							borderColor="#FF5722"
-						/>
-					</div>
-					<div class={styles.spaceDiv} />
-					<div class={styles.inputContainerNumber}>
-						<SearchBar
-							placeholderText="Durability"
-							borderColor="#FF5722"
-						/>
-					</div>
-				</div>
-				<div class={styles.dataContainer}>
-					<SelectItems
-						labelText="Card Set"
-						itemsArray={cardSets}
-					/>
-				</div>
-				<div class={styles.dataContainer}>
-					<SelectItems
-						labelText="Locale (language)"
-						itemsArray={localeArray}
-					/>
-					<div class={styles.spaceDiv} />
-					<ToggleSwitch
-						labelText="Collectible"
-					/>
-				</div>	
+				<Tabs>
+					<Tab label="Search by Name">
+						<div class={styles.verticalDiv} />
+						<div class={styles.dataContainer}>
+							<div class={styles.inputContainerLarge}>
+								<SearchBar
+									placeholderText="Card Name"
+									borderColor="#FF5722"
+								/>
+							</div>
+						</div>
+						<div class={styles.dataContainer}>
+							<SelectItems
+								labelText="Locale (language)"
+								itemsArray={localeArray}
+							/>
+						</div>	
+					</Tab>
+					<Tab label="Search by Stats">
+						<div class={styles.verticalDiv} />
+						<div class={styles.dataContainer}>
+							<div class={styles.inputContainerNumber}>
+								<SearchBar
+									placeholderText="Mana Cost"
+									borderColor="#FF5722"
+								/>
+							</div>
+							<div class={styles.spaceDiv} />
+							<div class={styles.inputContainerNumber}>
+								<SearchBar
+									placeholderText="Attack"
+									borderColor="#FF5722"
+								/>
+							</div>
+							<div class={styles.spaceDiv} />
+							<div class={styles.inputContainerNumber}>
+								<SearchBar
+									placeholderText="Health"
+									borderColor="#FF5722"
+								/>
+							</div>
+							<div class={styles.spaceDiv} />
+							<div class={styles.inputContainerNumber}>
+								<SearchBar
+									placeholderText="Durability"
+									borderColor="#FF5722"
+								/>
+							</div>
+						</div>
+						<div class={styles.dataContainer}>
+							<SelectItems
+								labelText="Card Set"
+								itemsArray={cardSets}
+							/>
+							<div class={styles.spaceDiv} />
+							<ToggleSwitch
+								labelText="Collectible"
+							/>
+						</div>
+						<div class={styles.verticalDivSmall} />
+						<div class={styles.checkboxContainer}>
+							{qualityCheckboxes}
+						</div>
+						<div class={styles.dataContainer}>
+							<SelectItems
+								labelText="Locale (language)"
+								itemsArray={localeArray}
+							/>
+						</div>	
+					</Tab>
+				</Tabs>
 				<div class={styles.verticalDiv} />
 				<div class={styles.dataContainer}>
 					<RaisedButton 
